fa704b78e1b7f88b861c9e1f4e1ab5d1d4c1a1d8
Darko <djukicdarko58@gmail.com>
2023-05-05 16:45:31 +0200

    Created Hero components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import BgCar from '../../images/hero/hero-bg.png'
-
+import { AiFillCheckCircle } from 'react-icons/ai';
+import { MdKeyboardArrowRight } from 'react-icons/md';
 
 const Hero = () => {
   return (
     <section className='home section' id='home'>
-    <div className='home__container container '>
-        <div className='home__content '>
-        
-            <div className='home__data'>
-                
-                <h1 className='home__title'>Save big with our car rental</h1>
-                <h3 className='home__subtitle'>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</h3>
-                
-        
-                <a href="#book"><button className='button button-flex'>Book ride</button></a>
-                <a href="#more"><button className='button button-flex'>Learn more</button></a>
-            </div>
-            <div className='home__images'>
-                <img src={BgCar} alt="Car" className="home__img" />
-            </div>
+      <div className='home__container container grid'>
+      <div className='home__conent'>
+        <div className='home__data'>
+                  <h3 className='home__subtitle'>Plan your trip now</h3>
+                  <h1 className='home__title'>Save big with our <br/> car rental</h1>
+                  <p className='home__description'>Rent the car of your dreams. Unbeatable prices, unlimited miles,<br/> flexible pick-up options and much more.</p>
+                  
+                  <div className='home__buttons'>
+                      <button className='button button--flex home__button-ride'>Book ride <AiFillCheckCircle className='button-icons'/> </button>
+                      <button className='button button--flex home__button-more'>Learn more <MdKeyboardArrowRight className='button-icons'/> </button>
+                  </div>
+              </div>
+              
         </div>
+        <div className='home__images'>
+                  <img src={BgCar} alt="Car" className="home__img" />
+                  <div className="home__triangle home__triangle-3"></div>
+                  <div className="home__triangle home__triangle-2"></div>
+                  <div className="home__triangle home__triangle-1"></div>
+        </div> 
     </div>
 </section>
   )
--- src/components/Hero/NavBar.jsx
+++ src/components/Hero/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { MdClose } from "react-icons/md";
 import { FaBars } from "react-icons/fa";
+import Logo from '../../images/logo/logo.png'
 
 const NavBar = () => {
     window.addEventListener('scroll', function(){
@@ -20,42 +21,51 @@ const NavBar = () => {
     <header className='header'>
             <nav className='nav container'>
                 <a href='#home' className='nav__logo'>
-                    {/* <img src={} alt="Logo" className='nav__logo-img'/> */}
-                    CarRental
+                    <img src={Logo} alt="Logo" className='nav__logo-img'/>
                 </a>
 
                 <div className={Toogle ? "nav__menu show-menu": "nav__menu"}>
                     <ul className='nav__list '>
                         <li className='nav__item' onClick={()=> showMenu(!Toogle)}>
                             <a href='#home' onClick={() => setActiveNav('#home')} className={activeNav === "#home" ? "nav__link active-link" : "nav__link"}>
-                                NASLOVNA
+                                Home 
                             </a>
                         </li>
                         <li className='nav__item' onClick={()=> showMenu(!Toogle)}>
                             <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? "nav__link active-link" : "nav__link"}>
-                                O NAMA
+                                About
                             </a>
                         </li>
                         <li className='nav__item' onClick={()=> showMenu(!Toogle)}>
-                            <a href='#service' onClick={() => setActiveNav('#service')} className={activeNav === "#service" ? "nav__link active-link" : "nav__link"}>
-                                USLUGE
+                            <a href='#models' onClick={() => setActiveNav('#models')} className={activeNav === "#models" ? "nav__link active-link" : "nav__link"}>
+                                Vehicle Models
                             </a>
                         </li>
                         <li className='nav__item' onClick={()=> showMenu(!Toogle)}>
-                            <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === "#project" ? "nav__link active-link" : "nav__link"}>
-                                PROJEKTI
+                            <a href='#testominials' onClick={() => setActiveNav('#testominials')} className={activeNav === "#testominials" ? "nav__link active-link" : "nav__link"}>
+                                Testominials
+                            </a>
+                        </li>
+                        <li className='nav__item' onClick={()=> showMenu(!Toogle)}>
+                            <a href='#team' onClick={() => setActiveNav('#team')} className={activeNav === "#team" ? "nav__link active-link" : "nav__link"}>
+                                Our Team
                             </a>
                         </li>
                         <li className='nav__item' onClick={()=> showMenu(!Toogle)}>
                             <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? "nav__link active-link" : "nav__link"}>
-                                KONTAKT
+                                Contact
                             </a>
                         </li>
                     </ul>
+
                     
                     
                     <MdClose className="nav__close" onClick={()=> showMenu(!Toogle)}/>
                     
+                </div>
+                <div className={Toogle ? "nav__menu": "nav__menu"}>
+                    <button className='button nav__button__sign-in'>Sing In</button>
+                    <button className='button nav__button__register'>Register</button>
                 </div>
 
                 <div className='nav__toggle' onClick={()=> showMenu(!Toogle)}>
